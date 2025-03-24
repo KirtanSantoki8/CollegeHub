a995d011fb81a3d1aa9aee8e1e84a3f0e04064fa
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-03-24 10:26:42 +0530

    commit

--- courses_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE collegehub;
-USE collegehub;
-
-CREATE TABLE courses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(255) NOT NULL,  
-    semester VARCHAR(50) NOT NULL,   
-    subject VARCHAR(255) NOT NULL    
-);
